6963326eaf641aaab6c495f7b310f4ac2ea9a4bc
Jcoll05 <jcolla@uninorte.edu.co>
2024-09-15 19:12:55 -0500

    fixed Footer.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
       <footer className="flex justify-center grid grid-rows-1 border-2 border-black p-4 bg-[#101010] items-center">
         <div> <img src="img/Logo.png" alt="Header Image" className="flex justify-center w-auto h-32"/> </div>
-        <p className="text-center text-[#9d9d9d]"> &copy; Julian Coll 2024</p>
+        <p className="text-center text-[#9d9d9d]" style={{ fontFamily: 'Georgia, Times New Roman, Times, serif', fontSize: '10px' }}> &copy; 2024 By Julian Coll ... All right reserved</p>
       </footer>
     );
   };
