517bc6d68d537ae0fa0ee3c04a0358daadb6c345
Jcoll05 <jcolla@uninorte.edu.co>
2024-09-16 02:44:51 -0500

    Final commit.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
     return (
       <header className="border-black p-4 bg-[#101010] flex flex-col md:flex-row justify-between items-center"style={{ fontSize: '28px' }}>
         <div className="mb-4 md:mb-0"> 
-          <img src="img/Logo.png" alt="Header Image" className="w-auto h-32"/> 
+          <img src="src/img/Logo.png" alt="Header Image" className="w-auto h-32"/> 
         </div>
         <nav className="flex flex-col md:flex-row md:space-x-4 w-full md:w-auto">
           <a href="#current-diagnosis" className="flex items-center justify-center w-full md:w-[200px] h-12 md:h-[115px] text-white font-bold rounded-md hover:bg-[#a90000]">
