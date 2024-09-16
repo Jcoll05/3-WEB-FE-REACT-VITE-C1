0eabc593b3b8b276bd26f7cd0ddd0972a8fe1e31
Jcoll05 <jcolla@uninorte.edu.co>
2024-09-16 11:47:16 -0500

    Final commit.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,9 @@
+import Logo from "/src/img/Logo.png";
+
 const Footer = () => {
     return (
       <footer className="flex flex-col md:flex-row justify-center grid grid-rows-[auto] border border-[#5a5a5a] p-4 bg-[#101010] items-center">
-        <div> <img src="src/img/Logo.png" alt="Header Image" className="flex justify-center w-auto h-32"/> </div>
+        <div> <img src={Logo} alt="Header Image" className="flex justify-center w-auto h-32"/> </div>
         <p className="text-center text-[#9d9d9d]" style={{ fontFamily: 'Georgia, Times New Roman, Times, serif', fontSize: '10px' }}> &copy; 2024 By Julian Coll ... All right reserved</p>
       </footer>
     );
